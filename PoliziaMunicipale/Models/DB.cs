99810643efe7411bcf32325bab0c262cf3e32867
MethaKinde <meethabeelkinde@gmail.com>
2024-03-02 11:48:03 +0100

    Aggiungere i file di progetto.

---
+++ PoliziaMunicipale/Models/DB.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace PoliziaMunicipale.Models
+{
+    public class DB
+    {
+        public static string connectionString = "server=DESKTOP-ADFVTUS\\SQLEXPRESS; Initial Catalog=PoliziaMunicipale; Integrated Security=true; TrustServerCertificate=True";
+        public static SqlConnection conn = new SqlConnection(connectionString);
+
+    }
+}
